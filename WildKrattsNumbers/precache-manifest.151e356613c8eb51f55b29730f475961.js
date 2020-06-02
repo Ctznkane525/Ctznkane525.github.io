a5cd9f4ab9274f3b390e4e4aedb89b46be7d66d6
Ctznkane525 <Ctznkane525@yahoo.com>
2020-06-02 10:46:27 -0400

    Kratts

---
+++ WildKrattsNumbers/precache-manifest.151e356613c8eb51f55b29730f475961.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184beab3d171b2463def8acd1e2f37ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a7588f7834cb3014fd8",
+    "url": "./static/css/main.3bed7885.chunk.css"
+  },
+  {
+    "revision": "20772e29650cafbb9d73",
+    "url": "./static/js/2.ebc4b3e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ebc4b3e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7588f7834cb3014fd8",
+    "url": "./static/js/main.a6d0b76f.chunk.js"
+  },
+  {
+    "revision": "66ee45baab6f447da521",
+    "url": "./static/js/runtime-main.62b1e1cd.js"
+  },
+  {
+    "revision": "61192117a555383d8b9885154a73d3c0",
+    "url": "./static/media/elephant.61192117.png"
+  },
+  {
+    "revision": "4d4f5730c33d5e6a76c28f91b09ef6e3",
+    "url": "./static/media/header.4d4f5730.png"
+  },
+  {
+    "revision": "4b8b59c5f48f8ff8f4f6002c09e1a599",
+    "url": "./static/media/idontknow.4b8b59c5.png"
+  },
+  {
+    "revision": "ccd213abfc9317e7346314b2c2ba232a",
+    "url": "./static/media/walrus.ccd213ab.png"
+  },
+  {
+    "revision": "e9842cd2fec5a83871545a41d96d0402",
+    "url": "./static/media/wolf.e9842cd2.png"
+  }
+]);
